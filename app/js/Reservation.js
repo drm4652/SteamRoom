93f2a4bd9b25631e3139b1852696788b2f695f73
Daniel Moody <drm4652@rit.edu>
2015-09-29 13:06:53 -0400

    Admin pages

---
+++ app/js/Reservation.js
@@ -0,0 +1,8 @@
+var Reservation = function (user, status, type, date, duration, roomNum) {
+  this.user = user;
+  this.status = status;
+  this.type = type;
+  this.date = date;
+  this.duration = duration;
+  this.roomNum = roomNum;
+};
---
+++ app/js/Room.js
@@ -0,0 +1,6 @@
+var Room = function (roomNumber, phoneLine, teleconference) {
+  this.roomNumber = roomNumber;
+  this.phoneLine = phoneLine;
+  this.teleconference = teleconference;
+};
+
