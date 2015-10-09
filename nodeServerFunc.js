1377fe53ec7a1e629ae5fe568e57c9791ad4e989
Nicholas Weller <naw7961@steamroom.se.rit.edu>
2015-10-09 12:39:37 -0400

    rearranged a few things and removed some others, didn't touch anyone elses files:

---
+++ nodeServerFunc.js
@@ -0,0 +1,54 @@
+var mysql = require('mysql');
+var path = require('path');
+var express = require('express');
+var app = express();
+
+var connection = mysql.createConnection({
+  host: '127.0.0.1',
+  port: '3306',
+  user: 'naw7961',
+  socketPath: '/var/run/mysql/mysql.sock',
+  password: 'naw7961',
+  database: 'steamreserve',
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+});
+
+app.set('port', 3000); //setting port to 8000
+app.set('views', path.join(__dirname, 'views')); //this will have the views in the view folder
+app.set('view engine', 'jade'); //set jade as the templating engine
+app.use(express.static(path.join(__dirname, 'public'))); // set the public directory as our asset holder
+
+//This is used in order to retrieve a list of reservations of the
+//entered user and display them in a jquery accordion
+app.get('/views', (function(req, res) {
+  "use strict";
+  //var sql = 'SELECT * FROM reservations WHERE username = ?';
+  //sql = mysql.format(sql, username);
+  var sql = 'SELECT * FROM reservations WHERE username = "naw7961"';
+  connection.query(sql, function(err, results) {
+    if (err) {
+      console.error('error with query: ' + err.stack);
+      return 1;
+    }
+    if (results) {
+      res.render('reservations', {
+        reservations: results
+      });
+    }
+  });
+}));
+
+app.get('/', (function(req, res) {
+  "use strict";
+   })
+);
+
+
+
+app.listen(app.get('port'));
