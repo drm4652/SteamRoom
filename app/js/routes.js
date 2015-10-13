b63d838d2808f6fc0905137523a245a5cca9a1f8
helrumyc <naw7961@rit.edu>
2015-10-13 00:00:19 -0400

    set up part of app for mongoDB and MEAN fullstack

---
+++ app/js/routes.js
@@ -0,0 +1,52 @@
+var Reservation = require('./models/reservation');
+
+function getReservations(res) {
+	Reservation.find(function(err, reservations) {
+		if(err) {
+			res.send(err);
+		}
+		res.json(reservations);
+	});
+};
+
+module.exports = function(app) {
+	
+	//get all reservations
+	app.get('api/reservations', function(req, res) {
+		//use mongoose to get all reservations in database
+		getReservations(res);
+	});
+	
+	// create todo and send back all todos after creation
+	app.post('/api/reservations', function(req, res) {
+		//create a reservation, information comes from AJAX request from Angular
+		Reservation.create({
+			reserver : req.body.reserver,
+			reservedAs : req.body.reservedAs,
+			date : req.body.date
+		}, function(err, reservation) {
+			if(err) {
+				res.send(err);
+			}
+			//get the updated list of reservations when you create new one
+			//getReservations(res);
+		});
+	});
+	
+	//delete a reservation
+	app.delete('/api/reservations/:reserver_id', function(req, res) {
+		Reservation.remove({
+			_id : req.params.reservation_id
+		}, function(err, todo) {
+			if(err) {
+				res.send(err);
+			}
+			getReservations(res);
+		});
+	});
+	
+	//application
+	app.get('*', function(req, res) {
+		res.sendfile('./views/listReservations.html'); // load the single view file
+	});
+});
--- nodeServerFunc.js
+++ nodeServerFunc.js
@@ -1,28 +1,31 @@
-var mysql = require('mysql');
-var path = require('path');
+// set up =========================================
 var express = require('express');
-var app = express();
+var app = express();  //create app with express
+var mongoose = require('mongoose'); // mongoose for mongodb
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
-var connection = mysql.createConnection({
-  host: '127.0.0.1',
-  port: '3306',
-  user: 'naw7961',
-  socketPath: '/var/run/mysql/mysql.sock',
-  password: 'naw7961',
-  database: 'steamreserve',
-});
+// config =========================================
+mongoose.connect('mongodb://localhost:27017/localSteam'); //connect to local mongoDB
 
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-});
 
 app.set('port', 3000); //setting port to 8000
-app.set('views', path.join(__dirname, 'views')); //this will have the views in the view folder
-app.set('view engine', 'jade'); //set jade as the templating engine
-app.use(express.static(path.join(__dirname, 'public'))); // set the public directory as our asset holder
+//app.set('views', path.join(__dirname, 'views')); //this will have the views in the view folder
+app.use(morgan('dev')); // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'})); 
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+//app.set('view engine', 'jade'); //set jade as the templating engine
+app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override head in the request
+app.use(express.static(path.join(__dirname, 'views'))); // set the public directory as our asset holder
+
+// routes =========================================
+require('./app/js/routes.js')(app);
+
+// listen (start app with node nodeServerFunc.js)
+app.listen(app.get('port'));
+console.log("App listening on port " + app.get('port'));
 
 //This is used in order to retrieve a list of reservations of the
 //entered user and display them in a jquery accordion
@@ -48,7 +51,3 @@ app.get('/', (function(req, res) {
   "use strict";
    })
 );
-
-
-
-app.listen(app.get('port'));
